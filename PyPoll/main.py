98b1704a7b65871e957e459f0983491c3b5ade11
George Bigham <gabigham4@gmail.com>
2019-07-03 11:01:20 -0700

    comit 2st draft

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,48 +1,56 @@
-# code for PyBank main part of HW3
-# written by George Bigham 7/1/19
-
-import csv
 import os
+import csv
 
-# path to dataset
-budget_csv_path = os.path.join('.', 'Resources', 'budget_data.csv')
 
-# variables for output data
-changes = []
-greatest_inc = 0.0
-greatest_dec = 0.0
-g_inc_date = ""
-g_dec_date = ""
+vote_csv_path = "Resources/election_data.csv"
 
-# read file to gather data for analysis
-with open(budget_csv_path) as csvfile:
+# variable and dictionary for vote counts
+total_votes = 0
+candidates = {}
+winner = ""
+winner_votes = 0
+
+
+# read and agragate votes for candidates
+with open(vote_csv_path) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
-    
+    # remove header
     next(csvreader)
-    
-    for row in csvreader:
-        changes.append(float(row[1]))
-        
-        if float(row[1]) > greatest_inc:
-            greatest_inc = float(row[1])
-            g_inc_date   = row[0]
-        if float(row[1]) < greatest_dec:
-            greatest_dec = float(row[1])
-            g_dec_date   = row[0]
-            
 
-# Write analysis summary file
-with open("financial_analysis.txt", "w") as text_file:
-    print("Financial Analysis", file = text_file)
+    for row in csvreader:
+        total_votes += 1
+        if row[2] in candidates:
+            candidates[row[2]] += 1
+        else:
+            candidates[row[2]] = 1
+
+
+# find winner
+for key, value in candidates.items():
+    if value > winner_votes:
+        winner = key
+        winner_votes = value
+
+
+# write results to election_results.txt
+with open("election_results.txt", "w") as text_file:
+    print("Election Results", file = text_file)
     print("-----------------------------", file = text_file)
-    print(f"Total Months: {len(changes)}", file = text_file) 
-    print(f"Total: ${int(sum(changes))}", file = text_file)
-    print(f"Average Change: ${round(sum(changes)/len(changes), 2)}", file = text_file)
-    print(f"Greatest Increase in Profits: {g_inc_date} ${greatest_inc}", file = text_file)
-    print(f"Greatest Decrease in Profits: {g_dec_date} ${greatest_dec}", file = text_file)
-    
+    print(f"Total Votes: {total_votes}", file = text_file)
+    print("-----------------------------", file = text_file)
+    for key, value in candidates.items():
+        print(key + ": " + str( "%.2f%%" % (value/total_votes)%100 ) + " (" + str(value) + ")", file = text_file)
+    print("-----------------------------", file = text_file)        
+    print("Winner: " + winner, file = text_file)
+    print("-----------------------------", file = text_file)    
+
+
 # Display summary in command line
-with open('test.txt', 'r') as text:
+with open('election_results.txt', 'r') as text:
     print(text.read())
-    
-    
+
+
+
+
+# def print_line(file):
+#    print("-----------------------------", file = file)
